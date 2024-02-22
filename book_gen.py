cc7f1408d6c5d4f61f986cf945379d1576ec0c79
mnuryga <mnuryga@gmail.com>
2024-02-22 18:05:11 -0500

    update

--- book_gen.py
+++ book_gen.py
@@ -3,8 +3,8 @@ import numpy as np
 def main():
 	day = 0
 	for i in range(40):
-		print(f'\"Book {i}\",{day},{np.random.randint(low=10_000, high=350_000)},{int(np.clip(np.random.normal(loc=20_000, scale=10_000),5_000, 50_000))}')
-		day += np.random.randint(low=0, high=10)
+		print(f'\"Book {i}\",{day},{np.random.randint(low=10_000, high=350_000)},{int(np.clip(np.random.normal(loc=20_000, scale=10_000),5_000, 35_000))}')
+		day += np.random.randint(low=4, high=10)
 
 if __name__ == '__main__':
 	main()
